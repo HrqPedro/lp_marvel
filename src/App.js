f2ef6c839c950654944e3bbb76e75b7888be29b4
HrqPedro <119547107+HrqPedro@users.noreply.github.com>
2023-02-16 22:16:43 -0300

    arquivos
    arquivos para landing page.

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        <div className="center">
+          <div className="menu">
+            <div className="logo">
+              <h3>Quem vence essa batalha?</h3>
+            </div>
+            <div className="item-menu">
+              <a href="#">Sobre</a>
+            </div>
+          </div>
+
+
+          <div className="form">
+            <h2>Vote agora!</h2>
+            <form>              
+              <div className="items-form">
+                <input type="text"></input>
+              </div>
+            </form>
+          </div>
+        </div>
+
+
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
